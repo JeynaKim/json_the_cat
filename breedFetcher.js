07cff111de1596d327a34e62e0138c2cfca89020
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-13 01:42:14 +0000

    Testing Cats as a Service completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,18 @@
 const request = require("request");
-const breed = process.argv[2];
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  function (error, response, body) {
-    if (error) {
-      console.log(error);
-      return console.log("Giant Error!");
+
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function (error, response, body) {
+      if (error) {
+        return callback(error, null);
+      }
+
+      const data = JSON.parse(body);
+      if (data.length === 0) return callback("not found", null);
+      return callback(null, data);
     }
-    console.error("error:", error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    console.log("body:", body); // Print the HTML for the Google homepage.
-    const data = JSON.parse(body);
-    if (data.length === 0) console.log("no match breed found");
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
